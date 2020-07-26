6023183248847bc4624c17752fb3e74636f355fc
chaduvulaprasanth <chaduvulaprasanth1998@gmail.com>
2020-07-26 17:17:24 +0530

    intialised with test

---
+++ test/bottles_test.rb
@@ -0,0 +1,23 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative '../lib/bottles'
+
+
+class BottlesTest < MiniTest::Test
+  def test_the_first_verse
+    expected =
+      "99 bottles of the beer on the wall, " + 
+      "99 bottles of beer.\n" +   
+      "Take one down and pass it around , " + 
+      "98 bottles of beer on the wall.\n"
+    assert_equal expected, Bottles.new.verse(99)
+  end
+  def test_another_verse
+    expected = 
+      "3 bottles of the beer on the wall, " + 
+      "3 bottles of beer.\n" +   
+      "Take one down and pass it around , " + 
+      "2 bottles of beer on the wall.\n"
+    assert_equal expected, Bottles.new.verse(3)
+  end
+end
