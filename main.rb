e0d073832aa4fcf0f5c8f7edd926b1d0946cd5d9
chaduvulaprasanth <chaduvulaprasanth1998@gmail.com>
2020-07-28 18:10:36 +0530

    7th chaper manufacturing intelligence

--- main.rb
+++ main.rb
@@ -7,37 +7,43 @@
 # end
 
 
-# class BottleNumber
-#   attr_reader :number
-#   def initialize
-#     @number = "number"
-#   end
-#   def quantity
-#     puts  "he"
-#   end
+class BottleNumber
 
-#   def to_s
-#     "#{quantity}"
-#   end
-# end
+  def self.inherited(rec)
+    puts rec
+  end
+  attr_reader :number
+  def initialize
+    @number = "number"
+  end
 
-# class BottleNumber0 < BottleNumber
-#   def quantity
-#     puts "no more"
-#   end
-# end
+  def quantity
+    puts  "he"
+    
+  end
+
+  def to_s
+    "#{quantity}"
+  end
+end
+
+class BottleNumber0 < BottleNumber
+  def quantity
+    puts "no more"
+  end
+end
 
 # # (5).downto(0).each{ |n| bottle_number_for(n)}
 
-# def hello
-#   bb = BottleNumber.new
+def hello
+  bb = BottleNumber.new
 
-#   puts bb
-# end
+  puts bb
+end
 
-# hello
-hash = Hash.new("hi").merge(
-  0 => "zero",
-  1 => "one"
-)
-puts hash[5]
+hello
+# hash = Hash.new("hi").merge(
+#   0 => "zero",
+#   1 => "one"
+# )
+# puts hash[5]
